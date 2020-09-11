7139d8717c84325e6965db3433c3c0a84527a261
EnzoConti <enzonicolasconti@gmail.com>
2020-09-11 02:19:18 -0300

    agregue evento

--- private/conectar.php
+++ private/conectar.php
@@ -1,4 +1,4 @@
 <?php
-    $con=mysqli_connect("mysql://uwryuwhzrr3jkrle:qz0o5ZKLv4P43EclphgP@brdipe7m6zylju95sqlv-mysql.services.clever-cloud.com:3306/brdipe7m6zylju95sqlv","uwryuwhzrr3jkrle","qz0o5ZKLv4P43EclphgP","brdipe7m6zylju95sqlv");
+    $con=mysqli_connect("brdipe7m6zylju95sqlv-mysql.services.clever-cloud.com","uwryuwhzrr3jkrle","qz0o5ZKLv4P43EclphgP","brdipe7m6zylju95sqlv");
 
 ?>
