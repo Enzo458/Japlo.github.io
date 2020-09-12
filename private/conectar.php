9ee496a3acd997c10482152e7e407765a25dc395
EnzoConti <enzonicolasconti@gmail.com>
2020-09-11 22:11:18 -0300

    enviar peticion

--- private/conectar.php
+++ private/conectar.php
@@ -1,4 +1,3 @@
 <?php
     $con=mysqli_connect("brdipe7m6zylju95sqlv-mysql.services.clever-cloud.com","uwryuwhzrr3jkrle","qz0o5ZKLv4P43EclphgP","brdipe7m6zylju95sqlv");
-
 ?>
--- private/getProductos.php
+++ private/getProductos.php
@@ -1,21 +1,22 @@
 <?php
-    include("conectar.php");
+    include('conectar.php');
     $respuesta= new stdClass();
 
     $prodts=[];
     $i=0;
-    $sql="selec * from productos where Disponible=1";
+    $sql="SELECT * FROM productos";
     $result=mysqli_query($con,$sql);
     while ($row=mysqli_fetch_array($result)){
         $obj=new stdClass();
-        $obj->nomP=$row['NomP'];
-        $obj->desP=$row['DesP'];
-        $obj->precioP=$row['PrecioP'];
-        $obj->rutP=$row['RutaP'];
+        $obj->NomP=$row['NomP'];
+        $obj->DesP=$row['DesP'];
+        $obj->PrecioP=$row['PrecioP'];
+        $obj->RutaP=$row['RutaP'];
         $prodts[$i]=$obj;
         $i++;
     }
-    $respuesta->data=$prodts;
+    $respuesta->prodts=$prodts;
+    
     mysqli_close($con);
     header('Content-Type: application/json');
     echo json_encode($respuesta);
